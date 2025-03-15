7264b89e0ec5df5e2bec5df3cfd2f3c7d1740cbb
Kabir Roshan <82226971+KabirRoshan@users.noreply.github.com>
2025-03-15 17:21:29 +0000

    Create CountingValleys.js

---
+++ CountingValleys.js
@@ -0,0 +1,67 @@
+// https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+    let seaLevel = 0;
+    let countValleys = 0;
+    
+    for (let i = 0; i < steps; i++){
+        if (path[i] == 'D') {
+            seaLevel++;
+        } else if (path[i] == 'U') {
+            seaLevel--;
+        }
+        
+        if (seaLevel == 0 && path[i] == 'U') {
+            countValleys++;
+        }      
+    }
+    
+    return countValleys;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const steps = parseInt(readLine().trim(), 10);
+
+    const path = readLine();
+
+    const result = countingValleys(steps, path);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
