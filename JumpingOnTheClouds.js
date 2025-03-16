4b10c1221c2deb919993209906a7c297922c33c7
Kabir Roshan <82226971+KabirRoshan@users.noreply.github.com>
2025-03-16 14:36:16 +0000

    Create JumpingOnTheClouds.js

---
+++ JumpingOnTheClouds.js
@@ -0,0 +1,61 @@
+// https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+function jumpingOnClouds(c) {
+    let index = 0;
+    let numberOfJumps = 0;
+    
+    while (index < c.length - 1) {
+        if (index + 2 < c.length && c[index + 2] === 0) {
+            index += 2;
+        } else {
+            index += 1;
+        }
+        numberOfJumps++;
+    }
+    return numberOfJumps;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const c = readLine().replace(/\s+$/g, '').split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const result = jumpingOnClouds(c);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
